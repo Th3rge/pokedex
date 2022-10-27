4b7b527cdfd1da2b1e85c3e03ce0b8f8e3d5712f
therge fonseca <thergefonseca@gmail.com>
2022-10-27 14:34:44 -0300

    imagem estatica trocada por imagem dinamica e ajuste de dimensao de imagem

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useLayoutEffect } from 'react'
 import axios from 'axios'
 import PokeBackground from './img/pokedex.png'
 import './App.css'
@@ -8,8 +8,8 @@ import './App.css'
 function App() {
 
   const [result, setResult] = useState({})
-  const initialValue = 3;
-
+  const initialValue = 2;
+  const pokemonImg = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${initialValue}.gif`
 
   const GetPokemons = () => {
     axios.get(`https://pokeapi.co/api/v2/pokemon/${initialValue}`)
@@ -23,21 +23,27 @@ function App() {
   }
 
 
-  useEffect(()=>{
-      {GetPokemons()}
+  useLayoutEffect(()=>{
+      
+    GetPokemons()
 
   }, [initialValue])
 
 
 
-  result.id === initialValue ? console.log(result.id, result.name) : GetPokemons()
-
+  //result.id === initialValue ? console.log(result.id, result.name) : GetPokemons()
+console.log(result)
 
   
   return (
     <div className="App">
       <div className='main'>
-      <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/6.gif' alt='pokemon' className='pokemon_image'/>
+      <img src={pokemonImg} alt='pokemon' className='pokemon_image'/>
+        
+        <h1 className='pokemon_data'>
+          <span className='pokemon_number'>{result.order} - {result.name}</span>
+        </h1>
+        
         <img src={PokeBackground} alt='pokedex' className='pokedex'/>
         
       </div>
